3936aca55480fd2f9098af8ff8ac402066bb8170
CLHISTEK <85032018+nicolaschavesw@users.noreply.github.com>
2025-03-19 16:39:10 -0500

    level 1

--- Assets/Script/MenuController.cs
+++ Assets/Script/MenuController.cs
@@ -3,14 +3,14 @@ using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    public Button[] levelButtons; // Asigna los botones en el Inspector
+    public Button[] levelButtons; 
 
     void Start()
     {
         for (int i = 0; i < levelButtons.Length; i++)
         {
             bool played = SaveSystem.Instance.HasPlayedLevel(i);
-            levelButtons[i].interactable = played || i == 0; // El primer nivel siempre está disponible
+            levelButtons[i].interactable = played || i == 0;
         }
     }
 
